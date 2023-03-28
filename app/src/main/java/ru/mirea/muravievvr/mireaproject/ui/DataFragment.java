2305dd6a21ad8344a3e58b553cf38b6e0916b016
Murken-0 <muravievvlad.2002@gmail.com>
2023-03-28 15:24:02 +0300

    add link to about.google

--- app/src/main/java/ru/mirea/muravievvr/mireaproject/ui/DataFragment.java
+++ app/src/main/java/ru/mirea/muravievvr/mireaproject/ui/DataFragment.java
@@ -1,5 +1,7 @@
 package ru.mirea.muravievvr.mireaproject.ui;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +9,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import ru.mirea.muravievvr.mireaproject.R;
 
@@ -16,6 +20,7 @@ import ru.mirea.muravievvr.mireaproject.R;
  * create an instance of this fragment.
  */
 public class DataFragment extends Fragment {
+    private FloatingActionButton fab;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -60,7 +65,14 @@ public class DataFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_data, container, false);
+        View v = inflater.inflate(R.layout.fragment_data, container, false);
+        fab = (FloatingActionButton) v.findViewById(R.id.fab);
+        fab.setOnClickListener(v1 -> {
+            String url = "https://about.google/";
+            Intent i = new Intent(Intent.ACTION_VIEW);
+            i.setData(Uri.parse(url));
+            startActivity(i);
+        });
+        return v;
     }
 }
